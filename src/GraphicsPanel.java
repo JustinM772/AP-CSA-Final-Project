cb0bf34783d7b5d34e369dbcd246abff4ded6f10
BT_1N3_01 <sample@bths.edu>
2024-06-05 09:27:10 -0400

    fixed issue with line showing up

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -18,7 +18,7 @@ public class GraphicsPanel extends JPanel implements KeyListener, ActionListener
     private String restrictedArea;
     private Timer timer;
     private int time = 50;
-    private int num;
+    private int restrictedAreaNum;
     private ArrayList<String> directions;
 
     public GraphicsPanel(String name) {
@@ -122,6 +122,7 @@ public class GraphicsPanel extends JPanel implements KeyListener, ActionListener
                 creatures = new ArrayList<>();
                 enemies = new ArrayList<>();
                 directions = new ArrayList<>();
+                restrictedAreaNum = 0;
             }
         }
     }
@@ -146,59 +147,59 @@ public class GraphicsPanel extends JPanel implements KeyListener, ActionListener
             int y = (int) (Math.random() * 801) + 100;
             creatures.add(new Creature(x, y));
             if (time == 30) {
-                num = (int) (Math.random() * 4) + 1;
+                restrictedAreaNum = (int) (Math.random() * 4) + 1;
             }
-            if (num == 1) {
+            if (restrictedAreaNum == 1) {
                 restrictedArea = "LEFT";
-            } else if (num == 2) {
+            } else if (restrictedAreaNum == 2) {
                 restrictedArea = "RIGHT";
-            } else if (num == 3) {
+            } else if (restrictedAreaNum == 3) {
                 restrictedArea = "TOP";
-            } else if (num == 4) {
+            } else if (restrictedAreaNum == 4) {
                 restrictedArea = "BOTTOM";
             }
             if (time == 10) {
-                num = (int) (Math.random() * 4) + 1;
+                restrictedAreaNum = (int) (Math.random() * 4) + 1;
             }
             if (time <= 30 && time > 10) {
-                if (num == 1) {
+                if (restrictedAreaNum == 1) {
                     if (player.getX() <= 500) {
                         player.setHealth(player.getHealth() - 20);
                         restrictedArea = "LEFT";
                     }
-                } else if (num == 2) {
+                } else if (restrictedAreaNum == 2) {
                     if (player.getX() > 500) {
                         player.setHealth(player.getHealth() - 20);
                         restrictedArea = "RIGHT";
                     }
-                } else if (num == 3) {
+                } else if (restrictedAreaNum == 3) {
                     if (player.getY() <= 500) {
                         player.setHealth(player.getHealth() - 20);
                         restrictedArea = "TOP";
                     }
-                } else if (num == 4) {
+                } else if (restrictedAreaNum == 4) {
                     if (player.getY() > 500) {
                         player.setHealth(player.getHealth() - 20);
                         restrictedArea = "BOTTOM";
                     }
                 }
             } else if (time <= 10 && time > -10) {
-                if (num == 1) {
+                if (restrictedAreaNum == 1) {
                     if (player.getX() <= 500) {
                         player.setHealth(player.getHealth() - 20);
                         restrictedArea = "LEFT";
                     }
-                } else if (num == 2) {
+                } else if (restrictedAreaNum == 2) {
                     if (player.getX() > 500) {
                         player.setHealth(player.getHealth() - 20);
                         restrictedArea = "RIGHT";
                     }
-                } else if (num == 3) {
+                } else if (restrictedAreaNum == 3) {
                     if (player.getY() <= 500) {
                         player.setHealth(player.getHealth() - 20);
                         restrictedArea = "TOP";
                     }
-                } else if (num == 4) {
+                } else if (restrictedAreaNum == 4) {
                     if (player.getY() > 500) {
                         player.setHealth(player.getHealth() - 20);
                         restrictedArea = "BOTTOM";
